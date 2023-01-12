b713431792e569da404beffc94b30f0773f755d9
anngelicafernanda <devanngelc@gmail.com>
2023-01-12 18:32:49 -0300

    Css la barra de navegacion

--- components/header.js
+++ components/header.js
@@ -1,30 +1,49 @@
-import Image from "next/image"
-import Link from "next/link"
-
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "../styles/header.module.css";
 
 export default function Header() {
+  const router = useRouter();
+
   return (
-    <header>
-    <div className="contenedor">
-    <Image src="/img/logo.svg" width={300} height={40} alt="Imagen Logotipo"/>
-      <nav>
-<Link href="/">
-Inicio
-</Link>
-
-<Link href="/nosotros">
-Nosotros
-</Link>
-
-<Link href="/blog">
-Blog
-</Link>
-
-<Link href="/tienda">
-Tienda
-</Link>
-      </nav>
-    </div>
+    <header className={styles.header}>
+      <div className={`contenedor ${styles.barra}`}>
+        <Link href="/">
+        <Image
+          src="/img/logo.svg"
+          width={300}
+          height={40}
+          alt="Imagen Logotipo"
+        />
+        </Link>
+        <nav className={styles.navegacion}>
+          <Link
+            className={router.pathname === "/" ? styles.active : ""}
+            href="/"
+          >
+            Home
+          </Link>
+          <Link
+            className={router.pathname === "/nosotros" ? styles.active : ""}
+            href="/nosotros"
+          >
+            Nosotros
+          </Link>
+          <Link
+            className={router.pathname === "/blog" ? styles.active : ""}
+            href="/blog"
+          >
+            Blog
+          </Link>
+          <Link
+            className={router.pathname === "/tienda" ? styles.active : ""}
+            href="/tienda"
+          >
+            Tienda
+          </Link>
+        </nav>
+      </div>
     </header>
-  )
+  );
 }
