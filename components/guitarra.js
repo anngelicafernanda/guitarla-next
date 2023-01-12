a23d85b57f9cb307c083304ed5673aaaa8634ae4
anngelicafernanda <devanngelc@gmail.com>
2023-01-12 20:42:33 -0300

    Estilos

--- components/guitarra.js
+++ components/guitarra.js
@@ -1,22 +1,23 @@
 import Image from "next/image"
 import Link from "next/link"
+import styles from '../styles/guitarras.module.css'
 
 export default function Guitarra({guitarra}) {
   const { descripcion, images, nombre, precio, url} = guitarra
   
   return (
-    <div>
+    <div className={styles.guitarra}>
 <Image
 src={images.data.attributes.formats.medium.url} 
 alt={`Imagen Guitarra ${nombre}`}
 width={600}
 height={400}
 />
-<div>
-  <h3>{nombre}</h3>
-  <p>{descripcion}</p>
-  <p>$ {precio}</p>
-<Link href={`/guitarras/${url}`}>Ver Producto</Link>
+<div className={styles.contenido}>
+  <h3 >{nombre}</h3>
+  <p className={styles.descripcion}>{descripcion}</p>
+  <p className={styles.precio}>$ {precio}</p>
+<Link className={styles.enlace} href={`/guitarras/${url}`}>Ver Producto</Link>
 </div>
     </div>
   )
