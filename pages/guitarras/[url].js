ea7ffc3835502fc7dc810c2dc835d4b0ed3b6e53
anngelicafernanda <devanngelc@gmail.com>
2023-01-13 10:20:16 -0300

    Routing dinamico con getStaticsPaths y getStaticsProps

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -10,7 +10,24 @@ export default function Producto({guitarra}) {
   )
 }
 
-export async function getServerSideProps({query: {url}}) {
+export async function getStaticPaths(){
+  const respuesta = await fetch(`${process.env.API_URL}/guitarras`)
+  const {data} = await respuesta.json()
+
+  const paths = data.map(guitarra=>({
+    params:{
+      url: guitarra.attributes.url
+    }
+  }))
+
+  console.log(paths)
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({params: {url}}) {
 const respuesta = await fetch(`${process.env.API_URL}/guitarras?filters[url]=${url}&populate=images`)
 const {data: guitarra} = await respuesta.json() 
 
@@ -21,3 +38,16 @@ return{
         }
     }
 }
+
+
+// export async function getServerSideProps({query: {url}}) {
+//   const respuesta = await fetch(`${process.env.API_URL}/guitarras?filters[url]=${url}&populate=images`)
+//   const {data: guitarra} = await respuesta.json() 
+  
+//   console.log(guitarra)
+//   return{
+//           props:{
+//               guitarra
+//           }
+//       }
+//   }
